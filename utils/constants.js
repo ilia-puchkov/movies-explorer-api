60c22d5c9603cd5f1659175119ad4fe096a9379e
ilia-puchkov <reras@yandex.ru>
2023-05-30 15:21:09 +0300

    fix: r2

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { NODE_ENV, MONGO_URL } = process.env;
 
-const MONGO_URL_DEV = 'mongodb://localhost:27017/bitfilmsdb';
+const MONGO_URL_DEV = 'mongodb://0.0.0.0:27017/bitfilmsdb';
 const JWT_SECRET_DEV = 'dev-secret';
 
 const DB_URL = NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV;
